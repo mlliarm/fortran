e149b54e3d926d7cfb8c98c84b5c80c9ce4a70dc
milia <mlliarm@yandex.com>
2021-12-22 05:52:50 +0100

    brought up to today's standards and cleaned code

--- 00_Differentiate/diff.f
+++ 00_Differentiate/diff.f
@@ -1,44 +1,39 @@
 !Purpose: Calculation of the first and second derivative of the function f(x) 
 !
-!Author: MiLia
-! 
-!Contact: milia@protonmail.com
-!	     
+!Author: MiLia (2005-2009)
 !___________________________________________________________________________
 
+      program diafor
+      implicit none
 
-       program diafor
-	
-       implicit none
+!	  Definition of variables/constants
+      real*8 diff1, diff2, f, h, x ! Double Precision vars
+      integer i
+	  parameter(h=1E-5)
+	  external f
 
-!	Definition of variables/constants
-	
-	real*8 diff1,diff2,f,h,cnt		
-	parameter(h=1E-5)
-	external f
+!	  Opening files to be written.
+	  open(1, file='diff1.dat') !1st derivative
+	  open(2, file='diff2.dat') !2nd derivative
 
-!	Opening files to be written.
+        
+	  do i = 0, 190
+        x = 1 + dble(i)/10.0
+		diff1 = (f(x+h) - f(x-h)) / (2*h) !1st derivative
+	  	diff2 = (f(x+h) + f(x-h) - 2*f(x)) / h**2 !2nd derivative
+	  	write(1, *) x, diff1
+	  	write(2, *) x, diff2
+	  enddo
 
-	open(1,file='diff1.dat') !1st derivative
-	open(2,file='diff2.dat') !2nd derivative
+      close(1) ! Closing file 1
+ 	  close(2) ! Closing file 2
+	  stop
+      end
 
-	do cnt=1,20,0.1
-		diff1=(f(cnt+h)-f(cnt-h))/(2*h) !1st derivative
-	  	diff2=(f(cnt+h)+f(cnt-h)-2*f(cnt))/h**2 !2nd derivative
-	  	write(1,*) cnt,diff1
-	  	write(2,*) cnt,diff2
-	enddo
-	
-       	 close(1) ! Closing file 1
- 	 close(2) ! Closing file 2
-
-	stop
-       end
-
-!	Given function
-	real*8 function f(x)
-	 real*8 x
-		f= sin(x*x)*cos*(x*x*x)
-	 return 
-	end	
+!	  Given function
+	  real*8 function f(x)
+	  real*8 x
+	    f = sin(x*x) * cos(x*x*x)
+	  return 
+	  end
 
--- 00_Differentiate/diffsub.f
+++ 00_Differentiate/diffsub.f
@@ -1,16 +1,16 @@
       subroutine diff(f,p,h,n)
 
-      real*8 f(0:n)k,p(0:n),h
-      integer n,i
+      real*8 f(0:n), p(0:n), h
+      integer n, i
 
-	open (1,file='diff.out') 
+	  open (1, file='diff.out') 
 
-	p(1)=(f(2)-f(1))/h
-	p(n)=(f(n)-f(n-1))/h
+	  p(1) = (f(2) - f(1)) / h
+	  p(n) = (f(n) - f(n-1)) / h
 
-         do i=2,n-1
-	   p(i)=(f(i+1)-f(i-1))/2*h
-	 enddo
+      do i = 2, n-1
+	    p(i) = (f(i+1) - f(i-1)) / 2*h
+	  enddo
 
 	  return
 	  end
