29a6751d0a9f6542e7fa56552cfd064174ffd86d
milia <mlliarm@yandex.com>
2021-12-22 06:43:34 +0100

    more generic

--- 00_Differentiate/plot_results.sh
+++ 00_Differentiate/plot_results.sh
@@ -1,17 +1,29 @@
 # Used:
 # https://askubuntu.com/a/702042/342004
 # https://staff.itee.uq.edu.au/ksb/howto/gnuplot-pdf-howto.html
+# https://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO-8.html
+# https://tldp.org/LDP/abs/html/complexfunct.html
 #--------------------------------------------------------------
 #!/bin/bash
 
-minval=0
-maxval=4219
-gnuplot -persist << EOFMarker
-    set title "Plot of the function's sin(x**2)*cos(x**3) first derivative"
-    set terminal postscript landscape enhanced color dashed lw 1 "DejaVuSans" 12
-    set output "diff1.ps"
-    plot "./diff1.dat" using 1:2 with linespoints
-EOFMarker
+function plot_two_col_file (){
+    minval=0
+    maxval=4219
+    title = $1; input_file = $2; output_file = $3
+    gnuplot -persist <<-EOFMarker
+        set title "$title"
+        set terminal postscript landscape enhanced color dashed lw 1 "DejaVuSans" 12
+        set output "$output_filename"
+        plot "$input_file" using 1:2 with linespoints
+    EOFMarker
+    # Convert ps to pdf
+    ps2pdf output_filename
+    # Delete ps
+    rm output_filename
+}
 
-ps2pdf diff1.ps
-rm diff1.ps
+function main() {
+    plot_two_col_file "Plot of the function's sin(x**2)*cos(x**3) first derivative" "./diff1.dat" "diff1.ps"
+}
+
+plot_two_col_file "test" "./diff1.dat" "diff1.ps"
