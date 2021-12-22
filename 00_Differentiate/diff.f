8d8b88da5288b48c044cd412ad1158285dd7dc89
milia <mlliarm@yandex.com>
2021-12-22 06:01:43 +0100

    clean code 2

--- 00_Differentiate/diff.f
+++ 00_Differentiate/diff.f
@@ -9,30 +9,29 @@
 !     Definition of variables/constants
       real*8 diff1, diff2, f, h, x ! Double Precision vars
       integer i
-	parameter(h=1E-5)
-	external f
+      parameter(h=1E-5)
+      external f
 
-!	Opening files to be written.
-	open(1, file='diff1.dat') !1st derivative
-	open(2, file='diff2.dat') !2nd derivative
+!     Opening files to be written.
+      open(1, file='diff1.dat') !1st derivative
+      open(2, file='diff2.dat') !2nd derivative
 
-        
 	do i = 0, 190
       x = 1 + dble(i)/10.0
-		diff1 = (f(x+h) - f(x-h)) / (2*h) !1st derivative
-	  	diff2 = (f(x+h) + f(x-h) - 2*f(x)) / h**2 !2nd derivative
-	  	write(1, *) x, diff1
-	  	write(2, *) x, diff2
-	 enddo
+           diff1 = (f(x+h) - f(x-h)) / (2*h) !1st derivative
+           diff2 = (f(x+h) + f(x-h) - 2*f(x)) / h**2 !2nd derivative
+           write(1, *) x, diff1
+           write(2, *) x, diff2
+      enddo
 
       close(1) ! Closing file 1
- 	close(2) ! Closing file 2
-	stop
+      close(2) ! Closing file 2
+      stop
       end
 
-!	Given function
-	real*8 function f(x)
-	real*8 x
-		f = sin(x*x) * cos(x*x*x)
-	return 
-	end
+!     Given function
+      real*8 function f(x)
+      real*8 x
+            f = sin(x*x) * cos(x*x*x)
+      return
+      end
